a7c71277df53a49b74b5968e508c0c7e6a1e817f
sasakizhao <121649863@qq.com>
2021-06-20 12:20:46 +0800

    modify tcp client

--- tcp_study/TCPClient.py
+++ tcp_study/TCPClient.py
@@ -4,18 +4,17 @@ def sendTCP():
    ##192.168.50.237
     serverName = '192.168.50.66'
     serverPort = 12000
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
 
     while True:
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName, serverPort))
         content = input('Input lower message\n')
         print('content:', content)
         clientSocket.send(content.encode())
         modifiedMessage = clientSocket.recv(1024)
-        if content == '.exit':
-            clientSocket.close()
-            print('connect closed\n')
         print('From Server:', modifiedMessage.decode())
+        clientSocket.close()
+        print('connect closed\n')
 
 if __name__ == '__main__':
     sendTCP()
